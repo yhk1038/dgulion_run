7c029b670d111463fe34f92ba85c17647595c69a
EC2 Default User <ec2-user@ip-172-31-20-241.ap-northeast-1.compute.internal>
2016-02-22 10:32:43 +0000

    seed.rb BackUp 160222 19:32

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,5 @@
 QnaPost.create!([
-  {user_id: nil, title: "1", content: "1", secret: 0}
+  {user_id: nil, title: "1", content: "1", secret: false}
 ])
 Service.create!([
   {desc_short: "온라인 프로그래밍 교육", logo_url: "http://brand.likelion.net/assets/services/codelion-char-w-64c13a44291e84fb3b38c1a3ea2260353ea82df048bfd9bb59a25485c934572b.png", name: "코드라이언", link_url: "http://codelion.net", desc_long: "한국인터넷진흥원 KISA와 공동프로젝트 진행, NIPA 캠퍼스CEO 프로그램 선정 약 3천여명이 수강", is_live: true},
@@ -10,7 +10,6 @@ Service.create!([
   {desc_short: "네일아트 정보 플랫폼", logo_url: "http://brand.likelion.net/assets/services/gliter-6db726b2facfccf0f1bb15db89c77a16ce44c3eeb5af3e0b473b16058043091d.png", name: "Gliter", link_url: "http://gliter.me", desc_long: "뷰티 컨텐츠에서 예약까지", is_live: true},
   {desc_short: "해외 송금 서비스", logo_url: "http://brand.likelion.net/assets/services/sentbe-8dd4adeb5e4b85c3d03df9c3c0dce658e0ddea2a68eb35aec31845cb0df8db27.png", name: "SENTBE", link_url: "https://asia.sentbe.com", desc_long: "해외송금을 국내송금처럼", is_live: true},
   {desc_short: "대학생 간 재능연결 플랫폼", logo_url: "http://brand.likelion.net/assets/services/taling-4baff29d26faa346032e93d8011f2c71bc36c91bcee89817f6f796f8177fd068.png", name: "탈잉", link_url: "https://asia.sentbe.com", desc_long: "내 능력이 가치가 되는 곳", is_live: true},
-  {desc_short: "대학생을 위한 전자투표 시스템", logo_url: "http://likelion.net/images/services/votepeople.jpg", name: "보트피플", link_url: "http://vote.snu.ac.kr", desc_long: "서울대 총학 투표 및 10여개 주요 학내 투표 진행. 서울대 최초의 학생 온라인 투표", is_live: false},
-  {desc_short: "여럿이 시간 약속 잡을 때는?", logo_url: "http://likelion.net/images/services/togedule.jpg", name: "투게쥴", link_url: "http://togedule.com", desc_long: "친구끼리 비는 시간 찾아서 약속시간 후보 알려주기", is_live: false},
-  {desc_short: "건국이래 가장 야동 잘 찾아주는 사이트", logo_url: "http://likelion.net/images/services/avsearch.jpg", name: "직박구리", link_url: "http://jikbakguri.com", desc_long: "야동 평가/추천 서비스 평균 하루 방문자 n만", is_live: false}
+  {desc_short: "공예재료시장의 길잡이 웹서비스", logo_url: "https://scontent.xx.fbcdn.net/hphotos-xpf1/v/t1.0-9/10632693_1723163154636708_1131722484831864292_n.png?oh=419e4b0de503a30b54c0e991e3a62010&oe=5725169D", name: "공예하자", link_url: "http://www.gongyehaza.com", desc_long: "공예재료시장에 대한 정보공유 및 재료시장 지도서비스", is_live: true},
+  {desc_short: "소리 SNS", logo_url: "http://blogpfthumb.phinf.naver.net/20160202_273/microstream_1454400109655kgf4R_PNG/main.png", name: "MicroStream", link_url: "https://microstream.kr", desc_long: "소리를 녹음하여 전파하자", is_live: true}
 ])
